6556d14d30fe280858a4ca5589e95e06855067d3
Yannick <yannicklamprecht@live.de>
2017-07-10 19:43:25 +0200

    - some cleaning

--- main.cpp
+++ main.cpp
@@ -8,15 +8,13 @@
 int main(int argc, char *argv[]) {
 
 
-
     std::list<std::shared_ptr<TestCase>> benchmarks;
 
 
     benchmarks.push_back(std::shared_ptr<TestCase>(new LoadAndUnloadBenchmark()));
     benchmarks.push_back(std::shared_ptr<TestCase>(new EnableAndDisableBenchmark()));
 
-
-    int count = 100;
+    unsigned long count = 100;
 
     for (std::shared_ptr<TestCase> testCase: benchmarks) {
         testCase->runTestFully(count);
